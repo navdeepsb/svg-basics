424613a4592812bf3cbc4cee0ac1703174bd19aa
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-03-25 15:15:58 -0400

    Init commit
    Played around with SVG’s <path /> element and some translation
    (rotation mainly)

---
+++ main.js
@@ -0,0 +1,29 @@
+// ...
+let selector = document.createElement( "select" );
+let svgElems = document.getElementsByTagName( "svg" );
+
+// ...
+let showSvg = ( name ) => {
+	Array.prototype.forEach.call( svgElems, ( el ) =>   el.style.display = "none" );
+	document.getElementsByClassName( selector.value )[ 0 ].style.display = "block" ;
+};
+
+// ...
+let len = svgElems.length;
+Array.prototype.forEach.call( svgElems, ( el, i ) => {
+	let nameOfElem = el.classList.value.split( " " ).pop();
+	selector.innerHTML += `
+		<option value="${ nameOfElem }">
+			${ len - i }. ${ nameOfElem }
+		</option>
+		`;
+});
+
+// ...
+selector.classList.add( "selector" );
+selector.addEventListener( "change", showSvg );
+document.getElementById( "container" ).insertBefore(
+	selector,
+	document.getElementById( "canvas" )
+);
+showSvg();
