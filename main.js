f5b861d29a2b37798a55c74d38692ecb072b9607
Navdeep Singh Bagga <navdeepb3191@gmail.com>
2018-03-25 17:05:39 -0400

    Converted tabs to spaces
    ¯\_(ツ)_/¯

--- main.js
+++ main.js
@@ -4,26 +4,26 @@ let svgElems = document.getElementsByTagName( "svg" );
 
 // ...
 let showSvg = () => {
-	Array.prototype.forEach.call( svgElems, ( el ) =>   el.style.display = "none" );
-	document.getElementsByClassName( selector.value )[ 0 ].style.display = "block" ;
+    Array.prototype.forEach.call( svgElems, ( el ) =>   el.style.display = "none" );
+    document.getElementsByClassName( selector.value )[ 0 ].style.display = "block" ;
 };
 
 // ...
 let len = svgElems.length;
 Array.prototype.forEach.call( svgElems, ( el, i ) => {
-	let nameOfElem = el.classList.value.split( " " ).pop();
-	selector.innerHTML += `
-		<option value="${ nameOfElem }">
-			${ len - i }. ${ nameOfElem }
-		</option>
-		`;
+    let nameOfElem = el.classList.value.split( " " ).pop();
+    selector.innerHTML += `
+        <option value="${ nameOfElem }">
+            ${ len - i }. ${ nameOfElem }
+        </option>
+        `;
 });
 
 // ...
 selector.classList.add( "selector" );
 selector.addEventListener( "change", showSvg );
 document.getElementById( "container" ).insertBefore(
-	selector,
-	document.getElementById( "canvas" )
+    selector,
+    document.getElementById( "canvas" )
 );
 showSvg();
--- nav.js
+++ nav.js
@@ -1,10 +1,10 @@
 // ...
 const navOptions = [{
-	path: "path.html",
-	title: "Path basics"
+    path: "path.html",
+    title: "Path basics"
 },{
-	path: "transform.html",
-	title: "Transform basics"
+    path: "transform.html",
+    title: "Transform basics"
 }];
 const currPath = location.pathname;
 
@@ -15,19 +15,19 @@ let navElem = document.createElement( "div" );
 // ...
 navElem.classList.add( "nav" );
 navOptions.forEach( ( nav ) => {
-	link = document.createElement( "a" );
-	link.setAttribute( "href", nav.path );
-	link.innerText = nav.title;
-	link.classList.add( "link" );
-	if( currPath.indexOf( nav.path ) >= 0 ) {
-		link.classList.add( "link--active" );
-	}
+    link = document.createElement( "a" );
+    link.setAttribute( "href", nav.path );
+    link.innerText = nav.title;
+    link.classList.add( "link" );
+    if( currPath.indexOf( nav.path ) >= 0 ) {
+        link.classList.add( "link--active" );
+    }
 
-	navElem.appendChild( link );
+    navElem.appendChild( link );
 });
 
 // ...
 document.getElementById( "container" ).insertBefore(
-	navElem,
-	document.getElementsByClassName( "selector" )[ 0 ] || document.getElementById( "canvas" )
+    navElem,
+    document.getElementsByClassName( "selector" )[ 0 ] || document.getElementById( "canvas" )
 );
